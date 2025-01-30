ec8283608b50cc3617aa8b986b6a74734edbe2f1
Ayush Atindra <ayush.atindra.dev@gmail.com>
2025-01-31 01:37:09 +0530

    Create 100pattern.cpp

---
+++ 100pattern.cpp
@@ -0,0 +1,597 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // ****
+    // ****
+    // ****
+    // ****
+
+    // int n=4;
+    // for(int i =0;i<4;i++){
+    //     cout <<"****"<<endl;
+    // }
+
+    // *
+    // **
+    // ***
+    // ****
+
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout <<"*";
+    //     }
+    //     cout <<endl;
+    // }
+
+    // ****
+    // ***
+    // **
+    // *
+
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<4-i;j++){
+    //         cout <<"*";
+    //     }
+    //     cout <<endl;
+    // }
+
+    // 1
+    // 12
+    // 123
+    // 1234
+
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout <<j+1;
+    //     }
+    //     cout <<endl;
+    // }
+
+    // 1234
+    // 123
+    // 12
+    // 1
+
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<4-i;j++){
+    //         cout <<j+1;
+    //     }
+    //     cout <<endl;
+    // }
+
+    // 4321
+    // 321
+    // 21
+    // 1
+
+    //    *
+    //   ***
+    //  *****
+    // *******
+
+    // for(int i=0;i<4;i++)
+    // {
+    //     // spaces
+    //     for(int j=0;j<4-i;j++){
+    //         cout <<" ";
+    //     }
+    //     // star
+    //     for(int j=0;j<2*i+1;j++)
+    //     {
+    //         cout <<"*";
+    //     }
+    //     cout <<endl;
+
+    // ****
+    // *  *
+    // *  *
+    // ****
+
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<4;j++){
+    //         if(j==0||j==4-1||i==0 || i==3){
+    //             cout<<"*";
+    //         }
+    //         else{
+    //             cout <<" ";
+    //         }
+
+    //     }
+    //     cout <<endl;
+
+    // }
+
+    // *
+    // **
+    // * *
+    // ****
+
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<i+1;j++){
+
+    //         if(j==0||j==i||i==0||i==3){
+    //             cout <<"*";
+    //         }
+    //         else{
+    //             cout <<" ";
+    //         }
+    //     }
+    //     cout <<endl;
+    // }
+
+    //    1
+    //   222
+    //  33333
+    // 4444444
+
+    // for(int i=0;i<4;i++){
+    //     // spaces
+    //     for(int j=0;j<4-i-1;j++){
+    //         cout<<" ";
+    //     }
+    //     // star
+    //     for(int j=0;j<2*i+1;j++){
+    //         cout <<i+1;
+    //     }
+    //     cout <<endl;
+    // }
+
+    // *****
+    // *****
+    // *****
+
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<5;j++){
+    //         cout <<"*";
+    //         }
+    //         cout <<endl;
+
+    // }
+
+    // 1
+    // 22
+    // 333
+    // 4444
+
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<=i;j++)
+    //     {
+    //         cout <<i+1;
+    //     }
+    //     cout <<endl;
+    // }
+
+    // *****
+    // *   *
+    // *   *
+    // *   *
+    // *****
+
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<5;j++){
+    //         if(i==0||i==4||j==0||j==4){
+    //             cout <<"*";
+    //         }
+    //         else{
+    //             cout <<" ";
+    //         }
+
+    //     }
+    //     cout <<endl;
+    // }
+
+    //  *
+    // ***
+    //  *
+
+    // for(int i=0;i<5;i++){
+    // // spaces
+    // for(int j=0;j<5-i-1;j++){
+    //     cout <<" ";
+    // }
+    // // stars
+    // for(int j=0;j<2*i+1;j++){
+    //     cout <<"*";
+    // }
+    // cout <<endl;
+    // }
+
+    // for(int i=0;i<5;i++){
+    // // spaces
+    // for(int j=0;j<i;j++){
+    //     cout <<" ";
+    // }
+    // // stars
+    // for(int j=0;j<2*5-2*i-1;j++){
+    //     cout <<"*";
+    // }
+    // cout <<endl;
+    // }
+
+    //
+    //    *
+    //   **
+    //  ***
+    // ****
+
+    // for(int i=0;i<4;i++){
+    //         for(int j=0;j<4-i-1;j++){
+    //             cout <<" ";
+    //         }
+    //         for(int j=0;j<i+1;j++){
+    //             cout <<"*";
+    //         }
+    //         cout <<endl;
+    // }
+
+    // 1
+    // 23
+    // 456
+    // 78910
+
+    // int x=1;
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<i+1;j++){
+    //         cout<<x;
+    //         x++;
+    //     }
+    //     cout <<endl;
+    // }
+
+    //    *
+    //   * *
+    //  *   *
+    // *******
+
+    // for(int i=0;i<4;i++){
+    //     // spaces
+    //     for(int j=0;j<4-i-1;j++){
+    //         cout <<" ";
+    //     }
+
+    //     // stars
+    //     for(int j=0;j<2*i+1;j++){
+    //         if(j==0||j==2*i ||i==3){
+    //             cout <<"*";
+    //         }
+    //         else{
+    //             cout<<" ";
+    //         }
+
+    //     }
+    //     cout<<endl;
+
+    // }
+
+    //   *
+    //  ***
+    // *****
+    //  ***
+    //   *
+
+    // for(int i=0;i<4;i++){
+    //     // spaces
+    //     for(int j=0;j<4-i-1;j++){
+    //         cout <<" ";
+    //     }
+    //     // stars
+    //     for(int j=0;j<2*i+1;j++){
+    //         cout<<"*";
+    // }
+    // cout<<endl;
+    // }
+
+    // for(int i=0;i<4;i++){
+    //     // spaces
+    //     for(int j=0;j<i;j++){
+    //         cout <<" ";
+    //     }
+    //     // stars
+    //     for(int j=0;j<2*4-2*i-1;j++){
+    //         cout<<"*";
+    // }
+    // cout<<endl;
+    // }
+
+    // 1
+    // * *
+    // 1 1 1
+    // * * * *
+
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<i+1;j++){
+    //         if(i%2==1){
+    //             cout<<"* ";
+    //         }
+    //         else{
+    //             cout<<"1 ";
+    //     }
+
+    // }
+    // cout<<endl;
+
+    // }
+
+    // *   *
+    //  * *
+    //   *
+    //  * *
+    // *   *
+
+    // *****
+    // *****
+    // *****
+    // *****
+    // *****
+
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<5;j++){
+    //         cout <<"*";
+    //     }
+    //     cout <<endl;
+    // }
+
+    // ******
+    // ******
+    // ******
+
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<5;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // *****
+    // *   *
+    // *   *
+    // *   *
+    // *****
+
+    // for(int i=0;i<5;i++)
+    // {
+    //     for(int j=0;j<5;j++){
+    //         if(i==0||i==4||j==0||j==4){
+    //             cout<<"*";
+    //         }
+    //         else{
+    //             cout<<" ";
+    //         }
+
+    //     }
+    //     cout<<endl;
+    // }
+
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //     *
+    //    **
+    //   ***
+    //  ****
+    // *****
+    // for(int i=0;i<5;i++){
+    //     // spaces
+    //     for(int j=0;j<5-i-1;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=0;j<i+1;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // *****
+    //  ****
+    //   ***
+    //    **
+    //     *
+
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<5-i;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // *****
+    //  ****
+    //   ***
+    //    **
+    //     *
+
+    // for(int i=0;i<5;i++){
+    //     // spaces
+    //     for(int j=0;j<i;j++){
+    //         cout<<" ";
+    //     }
+    //     // stars
+    //     for(int j=0;j<5-i;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 1
+    // 22
+    // 333
+    // 4444
+    // 55555
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<i+1;j++){
+    //         cout<<i+1;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 1
+    // 12
+    // 123
+    // 1234
+    // 12345
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<i+1;j++){
+    //         cout<<j+1;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 12345
+    // 1234
+    // 123
+    // 12
+    // 1
+
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<5-i;j++){
+    //         cout<<j+1;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // cout<<"*";
+    // cout<<"*****";
+    // for(int i=0;i<5;i++){
+    //     cout<<"*"<<endl;
+    // }
+    // for(int i=0;i<5;i++){
+    //     cout<<i+1<<" ";
+    // }
+    // for(int i=0;i<5;i++){
+    //     cout<<i+1<<endl;
+    // }
+
+    // char ch ='A';
+    // for(int i=0;i<5;i++){
+    //     cout<<ch<<endl;
+    //     ch++;
+    // }
+    // ***
+    // **
+    // *
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<3-i;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 1
+    // 12
+    // 123
+
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<3-i;j++){
+    //         cout<<j+1;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // ABC
+    // AB
+    // A
+    // char ch ='A';
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<i+1;j++){
+    //             cout<<ch;
+    //             ch++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // **
+    // **
+    // for(int i=0;i<2;i++){
+    //     for(int j=0;j<2;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // ***
+    // ***
+    // ***
+
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<3;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 111
+    // 222
+    // 333
+
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<3;j++){
+    //         cout<<i+1;
+
+    //     }
+    //     cout<<endl;
+    // }
+
+    // AAA
+    // BBB
+    // CCC
+
+    // for(int i=0;i<3;i++){
+
+    //     for(int j=0;j<3;j++){
+    //       char  ch='A';
+    //         cout<<ch;
+    //         ch++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // ***
+    // * *
+    // ***
+
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<3;j++){
+    //         if(i==0||i==2||j==0||j==2){
+    //             cout<<"*";
+    //         }
+    //         else{
+    //             cout<<" ";
+    //         }
+
+    //     }
+    //     cout<<endl;
+    // }
+
+    // * *
+    //  *
+    // * *
+
+    // cout<<"*";
+    // cout<<"**";
+    // cout<<"***";
+    // cout<<1;
+    // cout<<12;
+    // cout<<123;
+    // cout<<"A";
+    // cout<<"AB";
+    // cout<<"*";
+
+    cout << "*";
+    cout << "**";
+    cout << "***";
+    cout << "1";
+    cout << 1;
+    cout << 12;
+    cout << "A";
+}
